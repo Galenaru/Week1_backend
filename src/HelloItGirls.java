7f59e1326ab966f75c85e7c3db119e7415a70476
Alena_Gal <galenaru@yandex.ru>
2023-07-30 22:31:18 +0300

    first commit

---
+++ src/HelloItGirls.java
@@ -0,0 +1,20 @@
+public class HelloItGirls {
+    public static void main(String[] args){
+        Hello();
+        Name();
+        Sum();
+    }
+    public static void Hello(){
+        System.out.println("Hello, IT Girls! Welcome to my repository)");
+    }
+    public static void Name(){
+        System.out.println("My name is Alyona and here I am starting to study as a backend developer");
+    }
+    public static void Sum(){
+        int x = 7;
+        int y = 289;
+        System.out.println(x*y);
+        System.out.println("It's 2023 now");
+    }
+}
+
